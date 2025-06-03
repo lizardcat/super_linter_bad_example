939bbe1b0e8a63a8d71d0e043fa1cfbba7852d42
lizardcat <alex.raza.ke@gmail.com>
2025-06-03 20:09:56 +0300

    Updating to fix the code

--- bad_game.py
+++ bad_game.py
@@ -1,13 +1,15 @@
 import random
- def play():
-  number = random.randint(1, 10)
+
+def play():
+    number = random.randint(1, 10)
     guess = 0
-   while guess != number:
-    guess = int(input("Guess a number 1-10: "))
-      if guess<number:
-    print("Too low!")
-  elif guess>number:
-   print("Too high!")
-      else:
-    print("Correct!")
+    while guess != number:
+        guess = int(input("Guess a number 1-10: "))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print("Correct!")
+
 play()
